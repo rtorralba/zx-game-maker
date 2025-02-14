d7fc607d28a8d7eb58e22371b3db4157a8262f6a
Juan Antonio Rubio García <juanantonio.rubiogarcia@outlook.com>
2025-02-14 17:27:56 +0100

    NextNote is 17 bytes away from Play. Player.asm is one byte less..

--- src/player.asm
+++ src/player.asm
@@ -4,20 +4,25 @@
 
 ; Si se va a usar desde fuera de ASM, antes de Play poner ORG Dirección para que calcule las
 ; direcciones al ensamblar
+; Cambios realizados para su uso en ZX-Game Maker
 
 ; Primero: en la dirección Play+$01 cargar el sonido a reproducir
 ;     LD   Play+$01,$03   POKE Play+1,3           Indica cargar el sonido 3
 ; Segundo: llamar a Play
 ;     CALL Play           RANDOMIZE USR Play      Pone en memoria la dirección del sonido 3
 ; Tercero: llamar a NextTone cuando sea preciso
-;     CALL NextTone       RANDOMIZE USR Play+12   Reproduce un bloque del efecto y sale
+;     CALL NextTone       RANDOMIZE USR Play+17   Reproduce un bloque del efecto y sale
 ;                                                 Si no hay ningún efecto cargado sale
 Play:
-  ld   bc, $00          ; BC = A, efecto a reproducir
+  ld   c, $00           ; En $00 se indicará el sonido a reproducir
+  ld   b, $00           ; BC = A, efecto a reproducir
   ld   hl,FXAddress     ; Dirección en la que están las direcciones de los efectos
   add  hl,bc
   add  hl,bc            ; HL = dirección donde está la dirección del sonido a reproducir
-  ld   (FX),hl          ; Guarda en memoria la dirección del sonido a reproducir
+  ld   c, (hl)
+  inc  hl
+  ld   b, (hl)          ; BC = dirección del sonido a reproducir
+  ld   (FX),bc          ; Guarda en memoria la dirección del sonido a reproducir
   ret
 
 NextNote:
@@ -30,7 +35,7 @@ NextNote:
   push ix
   push iy               ; Preserva los registros índice
   push hl
-  pop  ix               ; Carga en IX el valor de HL (dirección del efecto), ver línea 29
+  pop  ix               ; Carga en IX el valor de HL (dirección del efecto/bloque), ver línea 29
 
   ld   a, ($5C48)       ; Obtiene los atributos del borde
   rra
@@ -42,15 +47,15 @@ NextNote:
                                     ; OR $00, sustituye 0 por el valor de A, en líneas 102 y 150
 
 readData:
-  ld   a, (ix+0)        ; A = tipo de bloque
-  ld   c, (ix+1)
-  ld   b, (ix+2)        ; BC = Frames
-  ld   e, (ix+3)
-  ld   d, (ix+4)        ; DE = Frame lenght
+  ld   a, (ix+$00)      ; A = tipo de bloque
+  ld   c, (ix+$01)
+  ld   b, (ix+$02)      ; BC = Frames
+  ld   e, (ix+$03)
+  ld   d, (ix+$04)      ; DE = Frame lenght, estos tres parámetros son comunes en tono y ruido
   push de
-  pop  iy               ; Carga en iy el valor de DE, Frame lenght
+  pop  iy               ; Carga en IY el valor de DE, Frame lenght
 
-  dec  a                ; Decrementa A
+  dec  a                ; Decrementa A (Tipo de bloque)
   jr   z,sfxRoutineTone ; Si es 0, es tono, salta
   dec  a                ; Decrementa A
   jr   z,sfxRoutineNoise; Si es 0, es ruido, salta
@@ -61,7 +66,7 @@ endData:
   ret                   ; Sale de la rutina
    
 nextData:
-  add  ix,bc            ; Apunta IX al bloque siguiente, ver líneas 126 y 180
+  add  ix,bc            ; Apunta IX al bloque siguiente, ver líneas 126 y 180 para BC, líneas 37 y 38 para IX
   ld   (FX),ix          ; Guarda en memoria la dirección del siguiente bloque 
   jr   endData          ; Salta para salir de la rutina
 
@@ -75,24 +80,24 @@ nextData:
 ;                             Frecuencia
 ;  defw 65536,	65536,		    65535,	    32768,			33023
 sfxRoutineTone:
-  ld   e, (ix+$05)
-  ld   d, (ix+$06)      ; DE = Pitch, ver línea 38
+  ld   e, (ix+$05)      ; IX = dirección el bloque, ver líneas 37 y 38
+  ld   d, (ix+$06)      ; DE = Pitch
   ld   a, (ix+$09)      ; A = Duty
   ld   (sfxRoutineToneDuty+1),a ; Modifica la rutina con el valor de Duty
                                 ; CP $00, susituye 0 por el valor de A, ver línea 98
-  ld   hl,$00           ; HL = 0
-
+  ld   hl,$00           ; HL = 0. Para reproducir el sonido va sumando Pitch (frecuencia)
+                        ; y usa el byte alto para activar/desactivar EAR y altavoz interno
 sfxRT0:
-  push bc               ; Preserva BC (Frames), ver líneas 52 y 53
+  push bc               ; Preserva BC (Frames), ver líneas 51 y 52
   push iy
   pop  bc               ; Carga en BC el valor de IY (Frame lenght), ver líneas 55 y 56
 sfxRT1: ; Desde aquí la rutina tarda 88 t-sates hasta la línea 108 (sincronización).
   add  hl,de            ; Suma DE (Pitch) a HL (11 t-states)
   ld   a, h             ; Carga el byte alto en A (4 t-sates)
 sfxRoutineToneDuty:
-  cp   $00              ; Lo compara con el valor de Duty ($00 se modifica en las líneas 110 a 112) (7 t-sates) 
+  cp   $00              ; Lo compara con el valor de Duty ($00 se modifica en las líneas 110 a 112, se carga la primera vez en línea 86) (7 t-sates) 
   sbc  a, a             ; Resta A - A teniendo en cuenta el acarreo (4 t-states)
-  and  $10              ; Se queda con el bit 4, EAR y altavoz interno activo/desactivo (7 t-states)
+  and  $10              ; Se queda con el bit 4, EAR y altavoz interno activado/desactivado (7 t-states)
 sfxRoutineToneBorder:
   or   $00              ; Añade el color del borde (se modificó en las línea 45) (7 t-states)
   out  ($fe),a          ; Manda el valor al puerto 254 para activar/desactivar EAR y altavoz interno (11 t-sates)
@@ -109,14 +114,14 @@ sfxRoutineToneBorder:
   ld   c, (ix+$07)
   ld   b, (ix+$08)      ; BC = Pitch slide
   ex   de,hl            ; Intercambia los valores de DE y HL
-  add  hl,bc            ; Suma Pitch slide a Pitch
+  add  hl,bc            ; Suma Pitch slide a Pitch, Pitch se carga en líneas 83 y 84
   ex   de,hl            ; Intercambia los valores de DE y HL (DE = Pitch + Pitch slide)
 
   pop  bc               ; Recupera BC (Frames)
   dec  bc               ; Decrementa BC
   ld   a, b
   or   c                ; Comprueba si BC = 0
-  jr   nz,sfxRT0        ; Si no es 0, bucle hasta que Frame = 0
+  jr   nz,sfxRT0        ; Si no es 0, bucle hasta que Frames = 0
 
   ld   c, $0b           ; C = bytes a los que está el siguiente bloque, BC viene a 0
   jp   nextData
@@ -129,21 +134,21 @@ sfxRoutineToneBorder:
 ;       Frames  Frame lenght  Pitch y Pitch slide
 ; defw  65535,  65535,        33023
 sfxRoutineNoise:
-  ld   e, (ix+$05)      ; E = Pitch
+  ld   e, (ix+$05)      ; E = Pitch, IX = dirección el bloque, ver líneas 37 y 38
   ld   d, $01
   ld   h, d
-  ld   l, d             ; HL = $11
+  ld   l, d             ; HL = $11 (Dirección 17 de la ROM)
 sfxRN0:
   push bc               ; Preserva BC (Frames)
   push iy
-  pop  bc               ; Carga en BC el valor de IY (Frame lenght)
+  pop  bc               ; Carga en BC el valor de IY (Frame lenght), ver líneas 55 y 56
 sfxRN1: 
-; Desde aquí, la rutina tarda 88 t-states hasta la línea 168 si en la línea 153 D=0 y 112 si D<>0
+; Desde aquí, la rutina tarda 112 t-states hasta la línea 168 si en la línea 153 D=0 y 88 si D<>0
   ld   a, (hl)          ; A = Valor de la dirección a la que apunta HL (en los primeros 8Kb de la ROM) (7 t-states)
-  and  $10              ; Se queda con el bit 4, activa o desactiva el EAR (7 t-states)
+  and  $10              ; Se queda con el bit 4, activa o desactiva EAR y altavoz interno (7 t-states)
 sfxRoutineNoiseBorder:
-  or   $00              ; Añade el color del borde ($00 se modificó en las línea 46) (7 t-states)
-  out  ($fe),a          ; Manda el valor al puerto 254 para emitir el sonido (11 t-sates)
+  or   $00              ; Añade el color del borde ($00 se modificó en la línea 46) (7 t-states)
+  out  ($fe),a          ; Manda el valor al puerto 254 para activar/desactivar EAR y altavoz interno (11 t-sates)
   dec  d                ; Decrementa D (4 t-states)
   jp   z, sfxRN2        ; Si es 0 salta (10 t-states)
   nop                   ; (4 t-states)
@@ -179,4 +184,5 @@ FX:
 dw $0000
 
 FXAddress:
+; Archivo asm generado por BeepFX sin reproductor
 include "assets/fx/fx.asm"
