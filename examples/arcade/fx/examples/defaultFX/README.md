582421326f7e1a42a83a9d604a003fca950b15f5
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2025-07-02 21:41:28 +0200

    Add new assets for arcade game including sprites, tiles, music, and screens
    - Added new sprite images and corresponding ZXP files for arcade map.
    - Introduced new tile images and ZXP files for arcade map.
    - Included music files in PT3 format for game and title screens.
    - Created new screen files for ending, game over, HUD, intro, loading, and title screens.
    - Added HUD tileset images and ZXP files, along with a TMX file for HUD layout.

---
+++ examples/arcade/fx/examples/defaultFX/README.md
@@ -0,0 +1,5 @@
+# Efectos por defecto
+
+El archivo fx.spj es el archivo de proyecto para BeepFX que contiene los efectos de sonido por defecto, adaptados al reproductor por bloques para que de esta forma no interrumpan el flujo del juego.
+
+El archivo fx.asm contiene la compilación generada por BeepFX sin el reproductor.
---
+++ examples/arcade/fx/fx.asm
@@ -0,0 +1,103 @@
+SoundEffectsData:
+	defw SoundEffect0Data
+	defw SoundEffect1Data
+	defw SoundEffect2Data
+	defw SoundEffect3Data
+	defw SoundEffect4Data
+	defw SoundEffect5Data
+	defw SoundEffect6Data
+
+SoundEffect0Data:
+	defb 2 ;noise
+	defw 1,1000,5124
+	defb 1 ;tone
+	defw 1,500,100,10,120
+	defb 1 ;tone
+	defw 1,500,100,10,120
+	defb 1 ;tone
+	defw 1,500,100,10,120
+	defb 1 ;tone
+	defw 1,500,100,10,120
+	defb 0
+SoundEffect1Data:
+	defb 2 ;noise
+	defw 1,500,10
+	defb 1 ;tone
+	defw 5,100,400,65526,128
+	defb 2 ;noise
+	defw 1,500,1
+	defb 0
+SoundEffect2Data:
+	defb 2 ;noise
+	defw 1,500,1
+	defb 0
+SoundEffect3Data:
+	defb 1 ;tone
+	defw 1,500,500,100,384
+	defb 1 ;tone
+	defw 1,500,500,100,384
+	defb 1 ;tone
+	defw 1,500,500,100,384
+	defb 1 ;tone
+	defw 1,500,1000,100,64
+	defb 1 ;tone
+	defw 1,500,1000,100,64
+	defb 1 ;tone
+	defw 1,500,1000,100,64
+	defb 1 ;tone
+	defw 1,500,1000,100,64
+	defb 1 ;tone
+	defw 1,500,1700,100,64
+	defb 1 ;tone
+	defw 1,500,1700,100,64
+	defb 1 ;tone
+	defw 1,500,1700,100,64
+	defb 0
+SoundEffect4Data:
+	defb 1 ;tone
+	defw 1,500,500,100,128
+	defb 1 ;tone
+	defw 1,500,500,100,64
+	defb 1 ;tone
+	defw 1,500,500,100,16
+	defb 0
+SoundEffect5Data:
+	defb 1 ;tone
+	defw 1,500,400,0,128
+	defb 1 ;tone
+	defw 1,500,400,1000,16
+	defb 1 ;tone
+	defw 1,500,800,0,128
+	defb 1 ;tone
+	defw 1,500,600,0,16
+	defb 0
+SoundEffect6Data:
+	defb 1 ;tone
+	defw 1,500,800,0,128
+	defb 1 ;tone
+	defw 1,500,800,0,128
+	defb 1 ;tone
+	defw 1,500,800,0,128
+	defb 1 ;tone
+	defw 1,500,800,0,128
+	defb 1 ;tone
+	defw 1,500,1000,0,16
+	defb 1 ;tone
+	defw 1,500,1000,0,16
+	defb 1 ;tone
+	defw 1,500,1000,0,16
+	defb 1 ;tone
+	defw 1,500,1000,0,16
+	defb 1 ;tone
+	defw 1,500,1200,0,128
+	defb 1 ;tone
+	defw 1,500,1200,0,128
+	defb 1 ;tone
+	defw 1,500,1200,0,16
+	defb 1 ;tone
+	defw 1,500,1200,0,16
+	defb 1 ;tone
+	defw 1,500,1400,0,128
+	defb 1 ;tone
+	defw 1,500,1400,0,16
+	defb 0
