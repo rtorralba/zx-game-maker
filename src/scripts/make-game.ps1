b670f8a66ada62eec607076884b6fcd6ab1ad330
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2025-09-01 21:27:09 +0200

    refactor: remove virtual environment setup from make-game scripts

--- src/scripts/make-game.ps1
+++ src/scripts/make-game.ps1
@@ -1,17 +1,5 @@
 $python = Get-Command py -ErrorAction SilentlyContinue
 
-$venv = ".\venv\Scripts\Activate.ps1"
-
-if (-not (Test-Path $venv)) {
-    Write-Host "Creando entorno virtual venv..." -ForegroundColor Yellow
-    py -m venv venv
-}
-
-if (-not $env:VIRTUAL_ENV) {
-    Write-Host "Activando entorno virtual venv..." -ForegroundColor Yellow
-    .\venv\Scripts\Activate.ps1
-}
-
 py .\build.py $args
 
 Read-Host "Pulse una tecla para cerrar..."
--- src/scripts/make-game.sh
+++ src/scripts/make-game.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-# Crear entorno virtual si no existe
-if [ ! -d "venv" ]; then
-    echo "Creando entorno virtual venv..."
-    python -m venv venv
-fi
-
-# Activar entorno virtual si no está activado
-if [ -z "$VIRTUAL_ENV" ]; then
-    echo "Activando entorno virtual venv..."
-    source venv/bin/activate
-fi
-
 python ./build.py $@
