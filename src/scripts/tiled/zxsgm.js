986b6bf7d918e8c084b21d05e40227fbcec7f440
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2025-10-22 00:15:22 +0200

    feat remove forbidden sprites

--- src/scripts/tiled/zxsgm.js
+++ src/scripts/tiled/zxsgm.js
@@ -1,8 +1,18 @@
 tiled.log("Script ZXSGM started...");
 
+const forbiddenTiles = [1, 2, 3, 4, 5, 6, 7, 12, 13, 14, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47];
+
 function setClassByShape(obj) {
     // Sprites (shape === 0)
-    if (obj.shape === 0 && (!obj.className || obj.className === "")) {
+    if (obj.shape === 0) {
+        if (forbiddenTiles.includes(obj.tile.id)) {
+            tiled.alert("⚠️ Sprite no permitido.");
+            if (obj.layer && obj.layer.removeObject) {
+                obj.layer.removeObject(obj);
+                tiled.log("✗ Objeto eliminado: " + (obj.name || "ID:" + obj.id));
+            }
+            return false;
+        }
         if (obj.tile.id > 15) {
             obj.className = "ZXSGMEnemy";
             tiled.log("✓ Class 'ZXSGMEnemy' assigned to sprite: " + (obj.name || "ID:" + obj.id));
@@ -16,7 +26,7 @@ function setClassByShape(obj) {
         }
     }
     // Pointers (shape === 5)
-    else if (obj.shape === 5 && (!obj.className || obj.className === "")) {
+    else if (obj.shape === 5) {
         obj.className = "ZXSGMPointer";
         tiled.log("✓ Class 'ZXSGMPointer' assigned to pointer: " + (obj.name || "ID:" + obj.id));
         return true;
