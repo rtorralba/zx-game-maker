16478d97d08db6ea2d3a477bd3bbf48384b23219
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2025-10-22 02:55:52 +0200

    fix: update forbiddenSprites array and remove debugObject call in setClassByShape function

--- src/scripts/tiled/zxsgm.js
+++ src/scripts/tiled/zxsgm.js
@@ -1,11 +1,10 @@
 tiled.log("Script ZXSGM started...");
 
-const forbiddenSprites = [1, 2, 3, 4, 5, 6, 7, 9, 11, 12, 13, 14, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47];
+const forbiddenSprites = [1, 2, 3, 4, 5, 6, 7, 9, 11, 12, 13, 14, 15, 17, 19, 21, 23, 25, 27, 29, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47];
 
 function setClassByShape(obj) {
     // Sprites (shape === 0)
     if (obj.shape === 0) {
-        debugObject(obj); // Debugging line to inspect object properties
         // Si las propiedades x o y del objeto no son multiplos de 8 poner el valor multible de 8 más proximo
         if (obj.x % 8 !== 0 || obj.y % 8 !== 0) {
             obj.x = Math.round(obj.x / 8) * 8;
